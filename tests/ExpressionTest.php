7fb52d0f24b6df222f893c1d6d81f77f19367cc7
Mic <m28126584@gmail.com>
2024-03-12 21:19:49 +0800

    Add other test case

---
+++ tests/ExpressionTest.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace Tests;
+
+use PHPUnit\Framework\Attributes\CoversClass;
+use App\Classes\Expression;
+use PHPUnit\Framework\TestCase;
+
+#[CoversClass(Expression::class)]
+class ExpressionTest extends TestCase
+{
+    public function testRemoveInvalidParenthesisWithComplexExpression()
+    {
+        $this->assertEquals(
+            "1*(2+3*(4+5))",
+            (new Expression())->removeUnnecessaryParentheses("1*(2+(3*(4+5)))")
+        );
+    }
+
+    public function testRemoveInvalidParenthesisWithDivision()
+    {
+        $this->assertEquals(
+            "2 + 3 / -5",
+            (new Expression())->removeUnnecessaryParentheses("2 + (3 / -5)")
+        );
+    }
+
+    public function testRemoveInvalidParenthesisWithVariables()
+    {
+        $this->assertEquals(
+            "x+y+z+t+v+w",
+            (new Expression())->removeUnnecessaryParentheses("x+(y+z)+(t+(v+w))")
+        );
+    }
+
+    public function testRemoveInvalidParenthesisWithOtherCase()
+    {
+        $expression = new Expression();
+
+        $this->assertEquals(
+            "1+2",
+            $expression->removeUnnecessaryParentheses("(1+(2))")
+        );
+
+        $this->assertEquals(
+            "2-(2+3)",
+            $expression->removeUnnecessaryParentheses("2-(2+3)")
+        );
+
+        $this->assertEquals(
+            "2-(2+3)",
+            $expression->removeUnnecessaryParentheses("2-(2+3)")
+        );
+
+        $this->assertEquals(
+            "-2-(2+3)",
+            $expression->removeUnnecessaryParentheses("-(2)-(2+3)")
+        );
+
+        $this->assertEquals(
+            "-(2+3)",
+            $expression->removeUnnecessaryParentheses("-(2+3)")
+        );
+
+        $this->assertEquals(
+            "2*3/5",
+            $expression->removeUnnecessaryParentheses("2*(3/5)")
+        );
+
+        $this->assertEquals(
+            "2/(3/5)",
+            $expression->removeUnnecessaryParentheses("2/(3/5)")
+        );
+
+        $this->assertEquals(
+            "2/3",
+            $expression->removeUnnecessaryParentheses("2/(3)")
+        );
+
+        $this->assertEquals(
+            "5/6",
+            $expression->removeUnnecessaryParentheses("(5)/(6)")
+        );
+
+        $this->assertEquals(
+            "-5/7",
+            $expression->removeUnnecessaryParentheses("(-5)/7")
+        );
+
+        $this->assertEquals(
+            "-5*7",
+            $expression->removeUnnecessaryParentheses("(-5)*7")
+        );
+
+        $this->assertEquals(
+            "5*-3",
+            $expression->removeUnnecessaryParentheses("5*(-3)")
+        );
+    }
+}
